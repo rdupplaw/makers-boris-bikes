8dbcfb13d0278e0c1565a7161029f71c5cdbee63
Robert Dupplaw <dupplawrobert@gmail.com>
2021-04-27 15:20:06 +0000

    added #release_bike to DockingStation

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,3 +1,7 @@
 require 'DockingStation'
 describe DockingStation do
+  describe 'responds to #release_bike' do
+    subject { DockingStation.new }
+    it { is_expected.to respond_to(:release_bike) }
+  end
 end
