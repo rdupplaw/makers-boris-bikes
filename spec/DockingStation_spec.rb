02d755f4b1df2ddfc0ae739e5004baee0bdc3360
Robert Dupplaw <dupplawrobert@gmail.com>
2021-04-27 16:16:28 +0000

    DockingStation can now release bikes

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,4 +4,8 @@ describe DockingStation do
     subject { DockingStation.new }
     it { is_expected.to respond_to(:release_bike) }
   end
+  describe '#release_bike returns a Bike that responds to #working?' do
+    subject { DockingStation.new.release_bike }
+    it { is_expected.to respond_to(:working?) }
+  end
 end
