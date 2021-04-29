ae89f7952813c5ee62b97aa0f98f83db80fd1716
Robert Dupplaw <dupplawrobert@gmail.com>
2021-04-29 18:51:26 +0000

    can #report_broken on Bikes

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,10 @@
 class Bike
+  attr_reader :broken
+
   def working?
+  end
+
+  def report_broken
+    @broken = true
   end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,4 +3,12 @@ describe Bike do
   it 'responds to #working?' do 
     expect(Bike.new).to respond_to(:working?)
   end
+
+  describe '#report_broken' do
+    it 'sets @broken to true' do
+      bike = Bike.new
+      bike.report_broken
+      expect(bike.broken).to be_truthy
+    end
+  end
 end
